c4f1b831f86bcdf236d8431dd55dcdd458c899b2
Caleb Aviles <earthctzn1@gmail.com>
2019-04-21 17:38:29 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,8 +1,10 @@
+require 'pry'
 def starts_with_a_vowel?(word)
   word.start_with?(/\b[aeiou AEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.start_with?(/[\A(un)][\z(ng)]/) true?
+  binding.pry
+  text.start_with?(/[\A(un)][\z(ng)]/)
 end
 
