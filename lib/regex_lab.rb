3387df102fddf2fb320df6e9ab0b0b8ee890cfbc
Caleb Aviles <earthctzn1@gmail.com>
2019-04-21 17:10:49 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -2,3 +2,18 @@ def starts_with_a_vowel?(word)
   word.start_with?(/\b[aeiou AEIOU]/)
 end
 
+def words_starting_with_un_and_ending_with_ing(text)
+
+end
+
+def words_five_letters_long(text)
+
+end
+
+def first_word_capitalized_and_ends_with_punctuation?(text)
+
+end
+
+def valid_phone_number?(phone)
+
+end
