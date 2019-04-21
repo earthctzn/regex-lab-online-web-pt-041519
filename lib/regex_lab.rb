82a04563b9acf36516486504b48cc993d40e8d16
Caleb Aviles <earthctzn1@gmail.com>
2019-04-21 17:12:04 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,17 +3,6 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.start_with?(/\w+un)
 end
 
-def words_five_letters_long(text)
-
-end
-
-def first_word_capitalized_and_ends_with_punctuation?(text)
-
-end
-
-def valid_phone_number?(phone)
-
-end
